f88fc1b3a6b5b02e61d0cdc5c393f5d3a890c8d8
Michael Kreider <michaelkreider@gmail.com>
2023-05-25 08:40:20 -0400

    js refactor

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
+const processLine = (line) => {
+    const linksFound = line.match(/(?:www|https?)[^\s]+/g);
+    if (line === '' || linksFound === null) {
+        return '<br />';
+    }
+    const output = [];
+    for (let j = 0; j < linksFound.length; j += 1) {
+        let replace = linksFound[j];
+        if (replace.slice(-1) === ')') {
+            replace = replace.slice(0, -1);
+        }
+        if (!(linksFound[j].match(/(http(s?)):\/\//))) {
+            replace = `http://${linksFound[j]}`;
+        }
+        let linkText = replace.split('/')[2];
+        if (linkText.substring(0, 3) === 'www') {
+            linkText = linkText.replace('www.', '');
+        }
+        output.push(`<a href="${replace}" target="_blank">${linkText}</a><br />`);
+    }
+    return output.join('');
+};
+
 const convertLinks = (input) => {
     const lines = input.split('\n');
     const output = [];
     for (let i = 0; i < lines.length; i += 1) {
         const line = lines[i].trim();
-        if (line === '') {
-            output.push('<br />');
-        } else {
-            const linksFound = line.match(/(?:www|https?)[^\s]+/g);
-            if (linksFound != null) {
-                for (let j = 0; j < linksFound.length; j += 1) {
-                    let replace = linksFound[j];
-                    if (replace.slice(-1) === ')') {
-                        replace = replace.slice(0, -1);
-                    }
-                    if (!(linksFound[j].match(/(http(s?)):\/\//))) {
-                        replace = `http://${linksFound[j]}`;
-                    }
-                    let linkText = replace.split('/')[2];
-                    if (linkText.substring(0, 3) === 'www') {
-                        linkText = linkText.replace('www.', '');
-                    }
-                    output.push(`<a href="${replace}" target="_blank">${linkText}</a><br />`);
-                }
-            } else {
-                output.push('<br />');
-            }
-        }
+        output.push(processLine(line));
     }
     return output.join('');
 };
@@ -37,8 +38,8 @@ if (editTextArea.addEventListener) {
         const typedTxt = editTextArea.value;
         plainTextArea.innerHTML = convertLinks(typedTxt);
         localStorage.setItem('links', editTextArea.value);
-        document.getElementById('links').style.height = '1px';
-        document.getElementById('links').style.height = `${25 + editTextArea.scrollHeight}px`;
+        editTextArea.style.height = '1px';
+        editTextArea.style.height = `${25 + editTextArea.scrollHeight}px`;
     }, false);
 } else if (editTextArea.attachEvent) {
     editTextArea.attachEvent('onpropertychange', () => {
@@ -46,8 +47,8 @@ if (editTextArea.addEventListener) {
         plainTextArea.innerHTML = convertLinks(typedTxt);
         localStorage.setItem('links', editTextArea.value);
     });
-    document.getElementById('links').style.height = '1px';
-    document.getElementById('links').style.height = `${25 + editTextArea.scrollHeight}px`;
+    editTextArea.style.height = '1px';
+    editTextArea.style.height = `${25 + editTextArea.scrollHeight}px`;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
